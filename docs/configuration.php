8ec44b959f857e339a6a79d1e4d52261bd3d46f6
Andre Santiago-Neyra <125695806+Andre-asn@users.noreply.github.com>
2024-10-05 23:36:22 -0400

    create gitignore

--- docs/configuration.php
+++
@@ -1,6 +0,0 @@
-<?php
-$db = new mysqli('store-db.cjcu4yo0sjqd.us-east-2.rds.amazonaws.com', 'admin', 'nU4bkKrdFusUs0hHoHiA', 'ecommerce', '3306');
-if (mysqli_connect_errno()) {
-    echo 'Failure '. mysqli_connect_error();
-}
-?>
